24405ecb1a95c0e036a4c15714f819b105cdb288
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-06 21:45:25 -0500

    Doc:Created dcoumentation for the getCPUs() function

--- src/systemUtils.js
+++ src/systemUtils.js
@@ -1,5 +1,10 @@
 const os = require('os');
 
+/**
+ * Returns the number of CPUs on the machine
+ * @returns {number} The number of CPUs on the machine
+ */
+
 function getCPUs(){
 
     return os.cpus().length;
